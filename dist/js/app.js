3845d6e2f1919a036a9a6674a935d75cdeed08c7
vash-sort <44153367+vash-sort@users.noreply.github.com>
2018-10-15 10:56:18 +0400

    Add files via upload

---
+++ dist/js/app.js
@@ -0,0 +1,14 @@
+$('.slider1').slick({
+  dots: true,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 1,
+  slidesToScroll: 1
+});
+$('.slider2').slick({
+  dots: true,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 1,
+  slidesToScroll: 1
+});
